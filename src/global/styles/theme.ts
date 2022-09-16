4a65d04709d808dbc0cfc7198e9947769f1ef558
tiagobratzheck <tiagobratzheck@hotmail.com>
2022-09-16 10:23:43 -0300

    First commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,26 @@
+export default {
+    colors: {
+        primary: "#5636d3",
+
+        secondary: "#ff872c",
+        secondary_light: "rgba(255,135,44,0.4)",
+
+        success: "#12a454",
+        success_light: "rgba(18,164,84,0.4)",
+
+        attention: "#E83F5B",
+        attention_light: "rgba(232,63,91,0.4)",
+
+        shape: "#ffffff",
+        text_dark: "#000000",
+        title: "#363f5f",
+        text: "#969cb2",
+        background: "#f0f2f5",
+    },
+
+    fonts: {
+        regular: "Poppins_400Regular",
+        medium: "Poppins_500Medium",
+        bold: "Poppins_700Bold",
+    },
+};
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+    TransactionCard,
+    TransactionCardProps,
+} from "../../components/TransactionCard";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList,
+} from "./styles";
+
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
+export function Dashboard() {
+    const data: DataListProps[] = [
+        {
+            id: "1",
+            type: "positive",
+            title: "Remuneração",
+            amount: "R$ 3.980,59",
+            category: {
+                name: "Salário",
+                icon: "dollar-sign",
+            },
+            date: "15/08/2022",
+        },
+        {
+            id: "2",
+            type: "negative",
+            title: "Compras Muffato",
+            amount: "R$ 321,58",
+            category: {
+                name: "Alimentação",
+                icon: "coffee",
+            },
+            date: "15/08/2022",
+        },
+        {
+            id: "3",
+            type: "negative",
+            title: "Aluguel da casa",
+            amount: "R$ 1650,00",
+            category: {
+                name: "Aluguel",
+                icon: "shopping-bag",
+            },
+            date: "15/08/2022",
+        },
+    ];
+
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{
+                                uri: "https://avatars.githubusercontent.com/u/44751489?v=4",
+                            }}
+                        />
+                        <User>
+                            <UserGreeting>Olá!</UserGreeting>
+                            <UserName>Tiago</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+            <HighlightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 8.635,59"
+                    lastTransaction="Último lançamento dia 19 de setembro"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 4.115,22"
+                    lastTransaction="Último lançamento dia 28 de setembro"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 4.520,37"
+                    lastTransaction="29 de setembro"
+                />
+            </HighlightCards>
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList
+                    data={data}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
+        </Container>
+    );
+}
