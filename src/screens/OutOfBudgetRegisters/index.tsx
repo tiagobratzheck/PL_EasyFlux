fe240925b3397587024357b7f8c57f6015bf560b
tiagobratzheck <tiagobratzheck@hotmail.com>
2023-01-26 20:57:30 -0300

    commit
    commit

--- src/screens/OutOfBudgetRegisters/index.tsx
+++ src/screens/OutOfBudgetRegisters/index.tsx
@@ -1,17 +1,25 @@
 import firestore from "@react-native-firebase/firestore";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { Button } from "../../components/Forms/Button";
+import theme from "../../global/styles/theme";
 import { useAuth } from "../../hooks/auth";
 import { commonCategories } from "../../utils/categories";
-import { Container, Footer, Header, Title } from "./styles";
+import {
+    AmountCard, AmountEntry, Cards, CategoryEntry, Container,
+    ContainerCard, Description, Footer,
+    FooterCard,
+    Header, Icon, ListEntries, NameEntry, PercentCard, Separator, Title, TitleCard, TitleList, VisualCards
+} from "./styles";
 
 interface CategoryData {
     key: string;
     name: string;
     total: number;
     totalFormatted: string;
+    icon: string;
     color: string;
     percentNumber: number;
     percent: string;
@@ -30,6 +38,27 @@ export function OutOfBudgetRegisters({
     dateTransactions,
     closeOutOfBudgetRegisters,
 }: OutOfBudgetRegistersProps) {
+
+    const [totalBudget, setTotalBudget] = useState<string>('');
+    const [colors, setColors] = useState({
+        colorFirstCard: '#ffffff',
+        colorSecondCard: '#ffffff',
+        colorThirdCard: '#ffffff',
+        colorFourthCard: '#ffffff'
+    })
+    const [totalEntries, setTotalEntries] = useState({
+        value: '',
+        percentage: '',
+    })
+    const [totalEntriesInBudget, setTotalEntriesInBudget] = useState({
+        value: '',
+        percentage: '',
+    })
+    const [totalEntriesOutOfBudget, setTotalEntriesOutOfBudget] = useState({
+        value: '',
+        percentage: '',
+    })
+    const [entries, setEntries] = useState<CategoryData[]>([]);
     const { user } = useAuth();
 
     React.useEffect(() => {
@@ -63,52 +92,99 @@ export function OutOfBudgetRegisters({
                         period,
                         type,
                     };
-                });
-                
+                });                
+
                 const listBudget = dataTransformed.filter(
                     (register) => register.entryType === "budget"
                 );
-               
+
                 const sumBudgetRegisters = listBudget.reduce(
                     (acumullator: number, entry) => {
                         return acumullator + Number(entry.amount);
                     },
                     0
                 );
-              
+
+                setTotalBudget(sumBudgetRegisters.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                    }
+                ));
+
                 const listActuals = dataTransformed.filter(
                     (register) => register.entryType === "actual"
                 );
-              
+
                 const sumActualsRegisters = listActuals.reduce(
                     (acumullator: number, entry) => {
                         return acumullator + Number(entry.amount);
                     },
                     0
                 );
-               
+
+                setTotalEntries({
+                    value: sumActualsRegisters.toLocaleString(
+                        "pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                        }
+                    ),
+                    percentage: `${((sumActualsRegisters / sumBudgetRegisters) * 100).toFixed(2)}%`
+                })
+
                 const actualsInBugdet = listActuals.filter((register) =>
                     listBudgetCategories.includes(register.category)
                 );
-             
+
                 const sumActualsInBudget = actualsInBugdet.reduce(
                     (acumullator: number, entry) => {
                         return acumullator + Number(entry.amount);
                     },
                     0
                 );
-               
+
+                setTotalEntriesInBudget({
+                    value: sumActualsInBudget.toLocaleString(
+                        "pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                        }
+                    ),
+                    percentage: `${((sumActualsInBudget / sumActualsRegisters) * 100).toFixed(2)}%`
+                })
+
                 const actualsOutOfBugdet = listActuals.filter(
                     (register) =>
                         !listBudgetCategories.includes(register.category)
                 );
-              
+
                 const sumActualsOutOfBudget = actualsOutOfBugdet.reduce(
                     (acumullator: number, entry) => {
                         return acumullator + Number(entry.amount);
                     },
                     0
                 );
+                
+                setTotalEntriesOutOfBudget({
+                    value: sumActualsOutOfBudget.toLocaleString(
+                        "pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                        }
+                    ),
+                    percentage: `${((sumActualsOutOfBudget / sumActualsRegisters) * 100).toFixed(2)}%`
+                })
+
+                setColors({
+                    colorFirstCard : theme.colors.success,
+                    colorSecondCard: transactionType === 'positive' ? 
+                        theme.colors.secondary : theme.colors.attention,
+                    colorThirdCard: transactionType === 'positive' ? 
+                        theme.colors.secondary : theme.colors.attention,
+                    colorFourthCard: transactionType === 'positive' ?
+                        theme.colors.secondary : ( sumActualsOutOfBudget > 0 ? 
+                        theme.colors.attention : theme.colors.success )
+                })
 
                 const totalByCategory: CategoryData[] = [];
 
@@ -129,26 +205,29 @@ export function OutOfBudgetRegisters({
                             }
                         );
 
-                        const percentNumber = ((categorySum / sumActualsOutOfBudget) * 100);                        
+                        const percentNumber =
+                            (categorySum / sumActualsOutOfBudget) * 100;
                         const percent = `${(
                             (categorySum / sumActualsOutOfBudget) *
                             100
                         ).toFixed(2)}%`;
-
+                     
                         totalByCategory.push({
                             key: category.key,
                             name: category.name,
+                            icon: category.icon,
                             color: category.color,
                             total: categorySum,
                             totalFormatted,
                             percent,
-                            percentNumber
+                            percentNumber,
                         });
                     }
                 });
                 totalByCategory.sort((a, b) =>
                     a.total < b.total ? 1 : b.total < a.total ? -1 : 0
-                );                
+                );
+                setEntries(totalByCategory);
             });
     }, []);
 
@@ -161,6 +240,65 @@ export function OutOfBudgetRegisters({
                         : "Despesas não planejadas"}
                 </Title>
             </Header>
+            <Cards>
+            <VisualCards>
+                <ContainerCard color={colors.colorFirstCard}>
+                    <TitleCard>Orçamento</TitleCard>
+                    <FooterCard>
+                        <AmountCard>{totalBudget}</AmountCard>                        
+                    </FooterCard>
+                </ContainerCard>
+
+                <ContainerCard color={colors.colorSecondCard}>
+                    <TitleCard>{transactionType === 'positive' ? 'Total de entradas' : 'Total de Saídas'}</TitleCard>
+                    <FooterCard>
+                        <AmountCard>{totalEntries.value}</AmountCard>
+                        <PercentCard>{totalEntries.percentage}</PercentCard>
+                    </FooterCard>
+                </ContainerCard>
+            </VisualCards>
+
+            <VisualCards>
+                <ContainerCard color={colors.colorThirdCard}>
+                    <TitleCard>
+                        {transactionType === 'positive' ? 'Entradas com orçamento' : 'Saídas com orçamento'}
+                    </TitleCard>
+                    <FooterCard>
+                        <AmountCard>{totalEntriesInBudget.value}</AmountCard>
+                        <PercentCard>{totalEntriesInBudget.percentage}</PercentCard>
+                    </FooterCard>
+                </ContainerCard>
+
+                <ContainerCard color={colors.colorFourthCard}>
+                    <TitleCard>
+                        {transactionType === 'positive' ? 'Entradas sem orçamento' : 'Saídas sem orçamento'}
+                    </TitleCard>
+                    <FooterCard>
+                        <AmountCard>{totalEntriesOutOfBudget.value}</AmountCard>
+                        <PercentCard>{totalEntriesOutOfBudget.percentage}</PercentCard>
+                    </FooterCard>
+                </ContainerCard>
+            </VisualCards>
+            </Cards>
+
+            <ListEntries>
+                <TitleList>Lista de lançamentos</TitleList>
+                <FlatList
+                    data={entries}
+                    keyExtractor={(item) => item.key}                
+                    renderItem={({ item }) => (
+                        <CategoryEntry>
+                            <Description>
+                                <Icon name={item.icon} />
+                                <NameEntry>{item.name}</NameEntry>
+                            </Description>
+                            <AmountEntry>{item.totalFormatted}</AmountEntry>
+                        </CategoryEntry>
+                    )}
+                    ItemSeparatorComponent={() => <Separator />}
+                />
+            </ListEntries>
+
             <Footer>
                 <Button title="Voltar" onPress={closeOutOfBudgetRegisters} />
             </Footer>
--- src/screens/OutOfBudgetRegisters/styles.ts
+++ src/screens/OutOfBudgetRegisters/styles.ts
@@ -1,10 +1,17 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
+
 import styled from "styled-components/native";
+
+interface ContainerProps {
+    color: string;
+}
 
 export const Container = styled(GestureHandlerRootView)`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
+    justify-content: space-between;
 `;
 
 export const Header = styled.View`
@@ -22,7 +29,98 @@ export const Title = styled.Text`
     color: ${({ theme }) => theme.colors.shape};
 `;
 
+export const TitleList = styled.Text`
+    margin-top: 10px;
+    padding-left: 24px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(13)}px;
+    color: ${({ theme }) => theme.colors.text};
+`;
+
 export const Footer = styled.View`
     width: 100%;
     padding: 24px;
+`;
+
+export const Cards = styled.View`
+    justify-content: flex-start;
+`
+
+export const ListEntries = styled.View`
+    height: 40%;
+`
+
+export const VisualCards = styled.View`    
+    flex-direction: row;
+    margin-top: 10px;
+    padding-left: 24px;
+    padding-right: 24px;
+    justify-content: space-between;
+`;
+
+export const ContainerCard = styled.View<ContainerProps>`
+    width: 47%;
+    height: 80px;
+    background-color: ${({ theme }) => theme.colors.shape};
+    flex-direction: column;    
+    justify-content: space-between;
+    border-radius: 5px;
+    border-left-width: 6px;
+    border-left-color: ${({ color }) => color};
+    margin-bottom: 9px;
+    padding: 5px;
+`;
+
+export const TitleCard = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(13)}px;
+`;
+
+export const FooterCard = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const AmountCard = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(12)}px;
+`;
+
+export const PercentCard = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(9)}px;
+`
+
+export const CategoryEntry = styled.View` 
+    padding: 12px 24px 12px 24px; 
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Description = styled.View`
+    flex-direction: row;
+`;
+
+export const Icon = styled(MaterialCommunityIcons)`
+    font-size: ${RFValue(18)}px;
+    margin-right: 16px;
+`;
+
+export const NameEntry = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(13)}px;   
+`;
+
+export const AmountEntry = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(13)}px; 
+`;
+
+export const Separator = styled.View`
+    height: 0px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.title};
 `;
